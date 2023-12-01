a06edcd0b64c770de450cb7f9c9e824b34a0eef7
abubakrali10 <bakrali175@yahoo.com>
2023-12-01 03:10:52 +0200

    create holberton course and add getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(l) {
+    if (typeof l !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = l;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(s) {
+    if (!Array.isArray(s)) {
+      throw TypeError('Students must be array');
+    }
+    this._students = s;
+  }
+}
