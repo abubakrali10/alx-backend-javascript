0b45f83d6f376e169902eed30fde4f70e774d9dc
abubakrali10 <bakrali175@yahoo.com>
2024-02-20 06:10:33 +0200

    create simple nodejs server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  res.end('Hello Holberton School!');
+});
+
+const port = 1245;
+
+server.listen(port);
+
+module.exports = server;
